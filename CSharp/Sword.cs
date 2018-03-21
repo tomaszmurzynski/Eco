d3503fe9611e15380ad7b40a8e464d7d13155940
tomaszmurzynski <tomasz.murzynski@gmail.com>
2018-03-21 09:45:52 +0100

    Add files via upload
    Added new script

---
+++ CSharp/Sword.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationClip BasicAttack;
+
+ 
+    public AnimationClip Idle;
+
+    private Animation anim;
+
+    void Start()
+    {
+        // przypisanie zmiennej anim komponentu Animation 
+        anim = GetComponent<Animation>();
+        // początkowe przypisanie aktualnej animacji jako idle 
+        anim.clip = Idle;
+
+        // UWAGA : animacja idle w inspektorze właściwość Wrap Mode ma zaznaczone jako loop -> dzięki temu wykonuje się w kółko
+        // rozpoczęcie animacji 
+        anim.Play();
+    }
+
+    void Update()
+    {
+        // jeśli gracz nacisnal LPM
+        if (Input.GetMouseButtonDown(0))
+        {
+            // przypisanie aktualnej animacji jako basic attack
+            anim.clip = BasicAttack;
+            // rozpoczęcie animacji
+            anim.Play();
+            // zakolejkowanie kolejnej animacji jako idle
+            anim.PlayQueued(Idle.name);
+        }
+
+        // jeśli gracz nacisnal PPM
+        if (Input.GetMouseButtonDown(1))
+        {
+            // dodać animacje silego ataku
+        }
+    }
+}
---
+++ CSharp/UnitHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour
+{
+    public int HealthPoints = 100;
+    public void TakeHealth(int hp)
+    {
+        HealthPoints -= hp;
+
+        if (HealthPoints <= 0)
+        {
+            Debug.Log("Dead");
+            Destroy(gameObject);
+        }
+    }
+}
