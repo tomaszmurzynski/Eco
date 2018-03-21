a05dc796ad88114f44fc725be16d77c26a131b28
tomaszmurzynski <tomasz.murzynski@gmail.com>
2018-03-21 09:46:57 +0100

    Add files via upload
    Added new script

---
+++ CSharp/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, transform.position.y, target.position.z - 6f), 5f * Time.deltaTime);
+    }
+}
---
+++ CSharp/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+        }
+     //   Destroy(other.gameObject);
+
+    }
+}
