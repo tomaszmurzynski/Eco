841531677a6e97144e049c48ffcc16af3ecd179b
tomaszmurzynski <tomasz.murzynski@gmail.com>
2018-03-21 01:18:35 +0100

    Add files via upload
    Create new branch

---
+++ Algorytmy i złożoność obliczeniowa/Algorytm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy_i_złożoność_obliczeniowa
+{
+    public class Algorytm
+    {
+        public static void SortowanieBabelkowe()
+        {
+            int n = 5;
+            int[] a = new int[] { 1, 3, 5, 7, 9 };
+            int i, j, k;
+            for (i = 1; i < n; i++)
+            {
+                k = a[i];
+                for (j = i; j > 0 && (a[j - 1] > k); j--)
+                {
+                    a[j] = a[j - 1];
+                    a[j] = k;
+                }
+            }
+        }
+    }
+}
---
+++ Algorytmy i złożoność obliczeniowa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytmy_i_złożoność_obliczeniowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+}
